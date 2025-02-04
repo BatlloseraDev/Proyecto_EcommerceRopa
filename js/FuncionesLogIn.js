07862343b59f011efadae779c033a930b254abd2
BatlloseraDev <batlloseradeveloper@gmail.com>
2025-02-04 17:57:18 +0100

    Arreglo de estilos y LogIn funcional
    Ahora se puede navegar entre las paginas que tenemos ademas de poder entrar en los disitintos users.

--- js/FuncionesLogIn.js
+++ js/FuncionesLogIn.js
@@ -1,1 +1,41 @@
-//Esta parte si quereis hacer algo decidmelo
+
+
+document.addEventListener("DOMContentLoaded", function(){
+
+    document.getElementById("login-form").addEventListener("submit", function(event){
+        event.preventDefault();
+
+        let email= document.getElementById("email").value;
+        let password = document.getElementById("password").value;
+
+        const userDataAdmin = {
+            email: "admin@admin.com", /*de momento como testeo*/
+            password: "admin"
+        };
+        const userDataVendedor = {
+            email: "vendedor@vendedor.com", /*de momento como testeo*/
+            password: "vendedor"
+        };
+        const userDataUser = {
+            email: "user@user.com", /*de momento como testeo*/
+            password: "user"
+        };
+
+        if(email === userDataAdmin.email && password === userDataAdmin.password){
+            window.location.href="Interfaz-Admin.html";
+        }
+        else if(email === userDataVendedor.email && password === userDataVendedor.password){
+            window.location.href="vendedor.html";
+        }
+        else if(email === userDataUser.email && password === userDataUser.password){
+            window.location.href="Cliente.html";
+        }
+        else{
+            document.getElementById("error-message").style.display = "block"; 
+        }
+
+
+    });
+
+
+});
